aaaca0f52ec13b3316ef984308b43b570af6115e
zieunx <wldms_1217@naver.com>
2024-09-09 23:22:47 +0900

    생산자 소비 문제 2

--- java-advance1/src/main/java/thread/bounded/BoundedMain.java
+++ java-advance1/src/main/java/thread/bounded/BoundedMain.java
@@ -11,11 +11,17 @@ public class BoundedMain {
     public static void main(String[] args) {
 //        BoundedQueue queue = new BoundedQueue1(2);
 //        BoundedQueue queue = new BoundedQueue2(2);
-        BoundedQueue queue = new BoundedQueue3(2);
+//        BoundedQueue queue = new BoundedQueue3(2);
+//        BoundedQueue queue = new BoundedQueue4(2);
+//        BoundedQueue queue = new BoundedQueue5(2);
+//        BoundedQueue queue = new BoundedQueue6_1(2);
+//        BoundedQueue queue = new BoundedQueue6_2(2);
+//        BoundedQueue queue = new BoundedQueue6_3(2);
+        BoundedQueue queue = new BoundedQueue6_4(2);
 
         // 2. 생산자, 소비자 실행 순서 선택, "반드시 하나만 선택!"
-//        producerFirst(queue); // 생산자 먼저 실행
-        consumerFirst(queue); // 소비자 먼저 실행
+        producerFirst(queue); // 생산자 먼저 실행
+//        consumerFirst(queue); // 소비자 먼저 실행
     }
 
     private static void producerFirst(BoundedQueue queue) {
---
+++ java-advance1/src/main/java/thread/bounded/BoundedQueue6_1.java
@@ -0,0 +1,43 @@
+package thread.bounded;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.MyLogger.log;
+
+public class BoundedQueue6_1 implements BoundedQueue {
+
+    private BlockingQueue<String> queue;
+
+    public BoundedQueue6_1(int max) {
+        this.queue = new ArrayBlockingQueue<>(max);
+    }
+
+    @Override
+    public void put(String data) {
+        try {
+            queue.put(data);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String take() {
+        try {
+            return queue.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return queue.toString();
+    }
+}
